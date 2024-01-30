193bbd0aaa0f08529a7d6bb4fc63e2e52e73d01d
JoonYong <joonyongpark99@gmail.com>
2024-01-30 23:57:09 +0900

    Add FirstController Add @GetMapping("/hello")

---
+++ spring_practice/src/main/java/com/example/spring_practice/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.spring_practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("username","joonyong");
+        return "hello";
+    }
+}
