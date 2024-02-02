3d35fbfd7090a2dfc929837a75a907299546ab89
JoonYong <joonyongpark99@gmail.com>
2024-02-02 21:35:32 +0900

    Add fruit.mustache

--- Spring_Practice/src/main/java/com/example/spring_practice/controller/SecondController.java
+++ Spring_Practice/src/main/java/com/example/spring_practice/controller/SecondController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecondController {
 
-    @GetMapping("/random")
+    @GetMapping("/randomFruit")
     public String qwer(Model model){
         String[] arr = {"Apple", "Banana", "Cherry", "Dingle Very", " Eggfruit"};
-
         int randomNum = (int) (Math.random() * arr.length);
 
-        model.addAttribute("random", arr[randomNum]);
-        return "random";
+        model.addAttribute("fruit", arr[randomNum]);
+        return "fruit";
+    }
+
+    @GetMapping("/randomNumber")
+    public String randomNumber(Model model){
+        int randomNum = (int) (Math.random() * 100);
+
+        model.addAttribute("number", randomNum);
+        return "number";
     }
 
 
